9a6428b977aa06fa595465938af96415c3bcff3a
unknown <pedrotalalayv6767@gmail.com>
2026-06-11 15:12:32 -0300

    feat(back): scaffold Spring Boot 3.5 com perfis dev (H2) e prod (Supabase) e jjwt

---
+++ backend/src/main/java/br/pucgo/agrosafra/AgrosafraApplication.java
@@ -0,0 +1,13 @@
+package br.pucgo.agrosafra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosafraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosafraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/pucgo/agrosafra/AgrosafraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucgo.agrosafra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosafraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
